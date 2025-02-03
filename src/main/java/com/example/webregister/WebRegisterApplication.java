418d16be58109e36d2caf38811700a6a13cd7e8c
ipon1207 <ipon_1207@yahoo.co.jp>
2025-02-03 21:29:45 +0900

    初期セットアップ

---
+++ src/main/java/com/example/webregister/WebRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.webregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webregister/WebRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
